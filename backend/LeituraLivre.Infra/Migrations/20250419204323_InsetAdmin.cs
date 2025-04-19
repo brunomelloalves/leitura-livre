9c177c9d86b93fedc74ad4abb0ccdbdde411d0ac
brunomelloalves <brunomelloalves@gmail.com>
2025-04-19 16:45:10 -0400

    Migration para inserir admin

---
+++ backend/LeituraLivre.Infra/Migrations/20250419204323_InsetAdmin.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeituraLivre.Infra.Migrations
+{
+    public partial class InsetAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO public.""Usuarios"" (
+                    ""Nome"", 
+                    ""Telefone"", 
+                    ""Email"", 
+                    ""NrImovel"", 
+                    ""Senha"", 
+                    ""Aprovado"", 
+                    ""NomeUsuario"", 
+                    ""Admin""
+                ) VALUES (
+                    'Administrador do Sistema',
+                    '00000000000',
+                    'admin@leituralivre.local',
+                    0,
+                    'admin',
+                    TRUE,
+                    'admin',
+                    TRUE
+                );
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM public.""Usuarios""
+                WHERE ""NomeUsuario"" = 'admin';
+            ");
+        }
+
+    }
+}
